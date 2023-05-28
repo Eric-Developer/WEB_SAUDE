0bee2d9d416804e01b52591091cbdde1d3e1f390
Eric <ericspsantos@gmail.com>
2023-05-28 05:16:57 -0300

    atualização

---
+++ assets/js/app.js
@@ -0,0 +1,38 @@
+
+        function clickMenu(){
+    if(itens.style.display == 'block'){
+    itens.style.display = 'none';
+    }
+   else{
+    itens.style.display = 'block';
+   }
+
+        }
+
+        const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event){
+    if(event.type === 'touchstart'){
+        event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    constactive = nav.classList.contains('active')
+    }
+ 
+}
+
+  //Script do mapa
+  var map = L.map('mapa').setView([-7.08484,-41.47252], 20);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  L.marker([-7.08484,-41.47252]).addTo(map)
+      .bindPopup('Localização')
+      .openPopup();
+
+      
+btnMobile.addEventListener('click',toggleMenu);
+btnMobile.addEventListener('touchstart',toggleMenu);
+
