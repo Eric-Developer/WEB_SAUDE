6cb9c649633b050f4c6af5b2fffe189966850841
Eric <ericspsantos@gmail.com>
2023-05-28 18:00:52 -0300

    adicionando função ao horário e as especialidades

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,18 +21,20 @@ function toggleMenu(event){
  
 }
 
-  //Script do mapa
-  var map = L.map('mapa').setView([-7.08484,-41.47252], 20);
-
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map);
-
-  L.marker([-7.08484,-41.47252]).addTo(map)
-      .bindPopup('Localização')
-      .openPopup();
-
       
 btnMobile.addEventListener('click',toggleMenu);
 btnMobile.addEventListener('touchstart',toggleMenu);
+
+
+ //Script do mapa
+ var map = L.map('mapa').setView([-7.08484,-41.47252], 20);
+
+ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+ }).addTo(map);
+
+ L.marker([-7.08484,-41.47252]).addTo(map)
+     .bindPopup('Localização')
+     .openPopup();
+
 
